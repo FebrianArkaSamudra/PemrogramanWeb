2c94279147691b01ab6a19f0e8888b7de5674ff5
FebrianArkaSamudra <144299755+FebrianArkaSamudra@users.noreply.github.com>
2024-11-21 11:50:58 +0700

    run

---
+++ Week13/koneksi.php
@@ -0,0 +1,21 @@
+<?php
+
+$serverName = "localhost"; 
+$database = "database_web";
+$username = "SA"; 
+$password = "Arka@12345"; 
+
+try {
+ 
+    $dsn = "sqlsrv:Server=$serverName;Database=$database";
+    $conn = new PDO($dsn, $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Koneksi berhasil!";
+} catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
+}
+
+?>
+
